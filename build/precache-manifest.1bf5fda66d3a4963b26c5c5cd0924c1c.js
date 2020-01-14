dfa071db2f21316eff7fd61c4f3dfb9b2ccda88a
Nilesh Bhatore <47231@hexaware.com>
2020-01-14 11:58:32 +0530

    deployment meeting

--- build/precache-manifest.1bf5fda66d3a4963b26c5c5cd0924c1c.js
+++ build/precache-manifest.1bf5fda66d3a4963b26c5c5cd0924c1c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "93953f34d109adfbfd8be45a0b22cfe2",
+    "revision": "24149c0fdd0da3d003f45d6d1f60ebe1",
     "url": "/index.html"
   },
   {
-    "revision": "49374170756c49c779bc",
+    "revision": "2eebfc75d41a82bb2e11",
     "url": "/static/css/main.14b32a47.chunk.css"
   },
   {
-    "revision": "616b614af2246fe3aa22",
-    "url": "/static/js/2.d7c34dc6.chunk.js"
+    "revision": "66853586c7484b483b30",
+    "url": "/static/js/2.03e80fff.chunk.js"
   },
   {
     "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
-    "url": "/static/js/2.d7c34dc6.chunk.js.LICENSE"
+    "url": "/static/js/2.03e80fff.chunk.js.LICENSE"
   },
   {
-    "revision": "49374170756c49c779bc",
-    "url": "/static/js/main.f0fe9c9a.chunk.js"
+    "revision": "2eebfc75d41a82bb2e11",
+    "url": "/static/js/main.7f996640.chunk.js"
   },
   {
     "revision": "651b67fab756abfdc094",
